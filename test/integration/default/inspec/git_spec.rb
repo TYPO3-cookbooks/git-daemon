d1903bafb6d7781b0acbcee3dfe520dc8b8f4a5b
Steffen Gebert <steffen.gebert@typo3.org>
2016-12-03 11:48:49 +0100

    [TESTS] Add tests

---
+++ test/integration/default/inspec/git_spec.rb
@@ -0,0 +1,23 @@
+control 'git-1' do
+  title 'Git Setup'
+  desc '
+    Check that git is installed and running
+  '
+
+  describe directory('/var/cache/git') do
+    it { should exist }
+  end
+
+  [9418].each do |listen_port|
+    describe port(listen_port) do
+      it { should be_listening }
+      its('protocols') { should include 'tcp6'}
+    end
+  end
+
+  # can clone a repository
+  describe command("git clone git://localhost/ /tmp/git-daemon_test-clone-$(date +%s)/") do
+    its('exit_status') { should eq 0 }
+  end
+
+end
